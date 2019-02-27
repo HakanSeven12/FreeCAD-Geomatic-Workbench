08f89dd0d09aec40af222eaaf9f08df3833d87ca
Hakan Seven <hakanseven12@gmail.com>
2019-02-27 17:23:56 +0300

    Update CreateSurface.py

--- CreateSurface.py
+++ CreateSurface.py
@@ -3,22 +3,33 @@ import FreeCADGui
 import Points
 import Mesh
 import ReverseEngineering as Reen
+from PySide import QtCore, QtGui
 
 class CreateSurface:
    def __init__(self):
-        self.planarMesh = []
         #Import *.ui file(s)
-        #self.IPFui = FreeCADGui.PySideUic.loadUi(FreeCAD.getHomePath() + "Mod/Geomatic/Resources/UI/ImportPointFile.ui")
+        self.IPFui = FreeCADGui.PySideUic.loadUi(FreeCAD.getHomePath() + "Mod/Geomatic/Resources/UI/CreateSurface.ui")
         
         #To Do List
+        self.IPFui.CreateB.clicked.connect(self.CreateSurface)
+        self.IPFui.CancelB.clicked.connect(self.IPFui.close)
 
    def GetResources(self):
-        return {'MenuText': 'Create Surface', 'ToolTip': 'Create surface from selected point group.'}
-  
+        return {'MenuText': 'Create Surface', 'ToolTip': 'Create surface from selected point group(s).'}
+
    def Activated(self):
+        self.IPFui.show()
+        self.planarMesh = []
+
+   def CreateSurface(self):
+        #Import UI variables
+        SurfaceNameLE = self.IPFui.SurfaceNameLE.text()
+        TLenghLE = float(self.IPFui.TLenghLE.text())*1000
+        OVLE = float(self.IPFui.OVLE.text())
+        #Create surface
         GroupName = FreeCAD.ActiveDocument.All_Points.Group
         OutList = FreeCAD.ActiveDocument.All_Points.OutList
-        limits = range(1,364)
+        limits = range(1,int(len(OutList)))
         Count = 0
 
         for i in limits:
@@ -30,8 +41,7 @@ class CreateSurface:
 
         Points.show(Points.Points(self.planarMesh))
         PointCloud = FreeCAD.ActiveDocument.Points001.Points
-        planarMeshObject=Reen.triangulate(PointCloud, 30000, 1000)
-        #planarMeshObject=Reen.triangulate(PointCloud, 10000, 3.5)
+        planarMeshObject=Reen.triangulate(PointCloud, TLenghLE, OVLE)
         Mesh.show(planarMeshObject)
 
 FreeCADGui.addCommand('Create Surface',CreateSurface()) 
