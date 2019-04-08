dd8528ac24ac49bc8d2b5e7c7b377faa31907c4e
Hakan Seven <hakanseven12@gmail.com>
2019-04-08 11:32:22 +0300

    Update ImportPointFile.py

--- ImportPointFile.py
+++ ImportPointFile.py
@@ -13,10 +13,12 @@ class ImportPointFile:
         self.IPFui.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
         
         #To Do List
-        self.IPFui.BrowseB.clicked.connect(self.BrowseFile)
-        self.IPFui.ImportB.clicked.connect(self.ImportFile)
-        self.IPFui.CancelB.clicked.connect(self.IPFui.close)
+        self.IPFui.AddB.clicked.connect(self.AddFile)
+        self.IPFui.RemoveB.clicked.connect(self.RemoveFile)
+        self.IPFui.SelectedFilesLW.itemSelectionChanged.connect(self.Preview)
         self.IPFui.CreateGroupChB.stateChanged.connect(self.CreateNewGroup)
+        self.IPFui.OKB.clicked.connect(self.ImportFile)
+        self.IPFui.CancelB.clicked.connect(self.IPFui.close)
 
    def GetResources(self):
         return {'MenuText': 'Import Point File', 'ToolTip': 'Import point file which include survey data.'}
@@ -35,23 +37,59 @@ class ImportPointFile:
             self.IPFui.SubGroupListCB.addItem(str(SubGroupName))
             Count = Count + 1
 
+   def AddFile(self):
+        self.FileList = QtGui.QFileDialog.getOpenFileNames(None, "Select one or more files to open", os.getenv("HOME"),'All Files (*.*)')
+        self.IPFui.SelectedFilesLW.addItems(self.FileList[0])
+
+   def RemoveFile(self):
+        listItems=self.IPFui.SelectedFilesLW.selectedItems() 
+        for item in listItems:
+           self.IPFui.SelectedFilesLW.takeItem(self.IPFui.SelectedFilesLW.row(item))
+
+   def Preview(self):
+        listItems=self.IPFui.SelectedFilesLW.selectedItems() 
+        head, tail = os.path.split(listItems[0].text())
+        self.IPFui.FileNameL.setText(tail)
+        #self.IPFui.PreviewLW.clear()
+        self.IPFui.PreviewLW.setRowCount(0)
+
+        PointName = self.IPFui.PointNameLE.text()
+        Northing = self.IPFui.NorthingLE.text()
+        Easting = self.IPFui.EastingLE.text()
+        Elevation = self.IPFui.ElevationLE.text()
+        Description = self.IPFui.DescriptionLE.text()
+
+        File=open(listItems[0].text(), 'r')
+        reader = csv.reader(File, delimiter=" ")
+        for i, row in enumerate( reader ):
+            PN = int(PointName)-1
+            N = int(Northing)-1
+            E = int(Easting)-1
+            Z = int(Elevation)-1
+            D = int(Description)-1
+
+            numRows = self.IPFui.PreviewLW.rowCount()
+            self.IPFui.PreviewLW.insertRow(numRows)
+
+            self.IPFui.PreviewLW.setItem(numRows, 0, QtGui.QTableWidgetItem(row[PN]))
+            self.IPFui.PreviewLW.setItem(numRows, 1, QtGui.QTableWidgetItem(row[N]))
+            self.IPFui.PreviewLW.setItem(numRows, 2, QtGui.QTableWidgetItem(row[E]))
+            self.IPFui.PreviewLW.setItem(numRows, 3, QtGui.QTableWidgetItem(row[Z]))
+            self.IPFui.PreviewLW.setItem(numRows, 4, QtGui.QTableWidgetItem(row[D]))
+
    def CreateNewGroup(self):
         if self.IPFui.CreateGroupChB.isChecked():
             self.IPFui.SubGroupListCB.setEditable(True)
         else:
             self.IPFui.SubGroupListCB.setEditable(False)
 
-   def BrowseFile(self):
-        self.FilePath = QtGui.QFileDialog.getOpenFileName(None, 'Select File', "", 'All Files (*.*)')
-        self.head, self.tail = os.path.split(self.FilePath[0])
-        self.IPFui.BrowseLE.setText(self.FilePath[0])
-
    def ImportFile(self):
         #Import UI variables
-        PointNameLE = self.IPFui.PointNameLE.text()
-        XLE = self.IPFui.XLE.text()
-        YLE = self.IPFui.YLE.text()
-        ZLE = self.IPFui.ZLE.text()
+        PointName = self.IPFui.PointNameLE.text()
+        Northing = self.IPFui.NorthingLE.text()
+        Easting = self.IPFui.EastingLE.text()
+        Elevation = self.IPFui.ElevationLE.text()
+        Description = self.IPFui.DescriptionLE.text()
         Index = self.IPFui.SubGroupListCB.currentIndex()
         CNG = self.IPFui.SubGroupListCB.currentText()
         SPG = self.GroupList[Index]
@@ -64,17 +102,22 @@ class ImportPointFile:
             SubGroup = FreeCAD.ActiveDocument.getObject(SPG)
 			
         #Read Points from file
-        File=open(self.head +"/"+ self.tail, 'r')
-        reader = csv.reader(File, delimiter=" ")
-        for i, row in enumerate( reader ):
-            pn = int(PointNameLE)-1
-            xx = int(XLE)-1
-            yy = int(YLE)-1
-            zz = int(ZLE)-1
-                
-            Point = Draft.makePoint(X = float(row[xx])*1000, Y = float(row[yy])*1000, Z = float(row[zz])*1000, color = (0.37,0.69,0.22) , name = "Point", point_size = 3)
-            Point.Label = str(row[pn])
-            SubGroup.addObject(Point)
+        Items = []
+        for i in range(self.IPFui.SelectedFilesLW.count()):
+            Items.append(self.IPFui.SelectedFilesLW.item(i))
+        Labels = [i.text() for i in Items]
+        for FilePath in Labels:
+            File=open(FilePath, 'r')
+            reader = csv.reader(File, delimiter=" ")
+            for i, row in enumerate( reader ):
+                PN = int(PointName)-1
+                N = int(Northing)-1
+                E = int(Easting)-1
+                Z = int(Elevation)-1
+
+                Point = Draft.makePoint(X = float(row[N])*1000, Y = float(row[E])*1000, Z = float(row[Z])*1000, color = (0.37,0.69,0.22) , name = "Point", point_size = 3)
+                Point.Label = str(row[PN])
+                SubGroup.addObject(Point)
         FreeCADGui.SendMsgToActiveView("ViewFit")
 
 FreeCADGui.addCommand('Import Point File',ImportPointFile()) 
